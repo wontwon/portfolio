05def332af2a42b8b27b8bbad4f8bd34c32abfd5
Tuan Duong <tuanduong15@gmail.com>
2017-04-06 22:13:17 -0700

    Add data for links displayed

---
+++ src/Data/link-data.js
@@ -0,0 +1,34 @@
+const linkData = [
+	{
+		icon: "envelope-o",
+		url: "mailto:tuanduong15@gmail.com?Subject=Hello%Tuan!",
+		text: "Email"
+	},
+	{
+		icon: "github",
+		url: "https://github.com/wontwon",
+		text: "Github"
+	},
+	{
+		icon: "gitlab",
+		url: "https://gitlab.com/wontwon",
+		text: "Gitlab"
+	},
+	{
+		icon: "linkedin-square",
+		url: "https://www.linkedin.com/in/tuanaduong/",
+		text: "LinkedIn"
+	},
+	{
+		icon: "twitter",
+		url: "https://twitter.com/tuananhduong",
+		text: "Twitter"
+	},
+	{
+		icon: "skype",
+		url: "skype:t.duong15",
+		text: "Skype"
+	}
+]
+
+export default linkData
